88437ca197d84296febbcf08bfd1c6495d90e5be
marioservini <31860@bridgend.ac.uk>
2021-11-13 13:50:38 +0000

    complete

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
 def main():
-    #write your code below this line
+    # write your code below this line
+    word = str(input('Give a string\n'))
+    num = int(input('Give an integer\n'))
+    fp = float(input('Give a float\n'))
+    bl = bool(input('Give a boolean\n'))
+    print('You gave the string', word)
+    print('You gave the integer', num)
+    print('You gave the float', fp)
+    print('You gave the boolean', bl)
+
 
 if __name__ == '__main__':
     main()
